92582dbea8c25646f140a9fa7ce9541fd2419f5e
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-07 09:24:24 +0530

    Create armstongnumberbetweeninterval.py

---
+++ beginnerlevel/armstongnumberbetweeninterval.py
@@ -0,0 +1,12 @@
+a=int(input())
+b=int(input())
+for i in range(a,b+1):
+   sum = 0
+   temp = i
+   while temp > 0:
+       digit = temp % 10
+       sum += digit ** 3
+       temp //= 10
+   if i== sum:
+       print(i)
+   
