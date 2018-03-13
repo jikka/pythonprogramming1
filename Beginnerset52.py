f11b09e4777e0adbcf375c57aff19345e24cce40
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-13 17:58:49 +0530

    Create Beginnerset52.py

---
+++ Beginnerset52.py
@@ -0,0 +1,16 @@
+
+string1=str(input(""))
+string2=str(input(""))
+count1=0
+count2=0
+if string1<=1000 && string2<=1000
+ for i in string1:
+      count1=count1+1
+ for j in string2:
+      count2=count2+1
+ if(count1<count2):
+      print(string2)
+ elif(count1==count2):
+      print(string1'+'string2)
+ else:
+      print(string1)
