b38eb2ac1d025cab9db7808c5b1c11d9ecb389b1
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-13 18:31:10 +0530

    Create Beginnerset57greatestnsmallestnumber.py

---
+++ beginnerlevel/Beginnerset57greatestnsmallestnumber.py
@@ -0,0 +1,14 @@
+list[]
+num=int(input())
+if(n<=10000):
+   for x in range(num):
+       numbers=int(input())
+       list.append(numbers)
+       
+   Print(max.list())
+   Print(min.list())
+       
+       
+ 
+      
+    
