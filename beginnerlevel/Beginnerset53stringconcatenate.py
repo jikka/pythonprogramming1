870becef1a0ecdfb9fd3b20c4b693fc9ad4d73c9
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-14 16:25:49 +0530

    Update Beginnerset53stringconcatenate.py

--- beginnerlevel/Beginnerset53stringconcatenate.py
+++ beginnerlevel/Beginnerset53stringconcatenate.py
@@ -1,4 +1,4 @@
 string1=str(input())
 string2=str(input())
-if((string1<=1000)&&(string2<=1000)):
-    print(string1'+'string2)
+if((len(string1)<=1000)and(len(string2)<=1000)):
+  print(string1+string2)
