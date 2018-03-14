192805c0d0f5c658ffd90623afe9b8f2fc5c72e7
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-14 19:15:47 +0530

    Create Beginnerset82findingvowel.py

---
+++ beginnerlevel/Beginnerset82findingvowel.py
@@ -0,0 +1,6 @@
+string=str(input(''))
+for x in string:
+    if(x=='A'or x=='a'or x=='E' or x=='e' or x=='I' or x=='i' or x=='O' or x=='o' or x=='U' or x==' u'):
+        print('yes')
+    else:
+        print('no')
