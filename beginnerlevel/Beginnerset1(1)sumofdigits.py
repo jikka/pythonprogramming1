3373335d86fc159fe401a7d6b7fad95204d277fd
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-18 20:43:03 +0530

    Update and rename Beginnerset1(1)sumofdigits.py to Beginnerset1(9)sumofdigits.py

--- beginnerlevel/Beginnerset1(1)sumofdigits.py
+++
@@ -1,9 +0,0 @@
-n=int(input())
-k=int(input())
-sum=0
-for x in range(1,n+1):
-  number=int(input())
-  while(x<=k):
-    sum=sum+number
-print(sum)
-  
---
+++ beginnerlevel/Beginnerset1(9)sumofdigits.py
@@ -0,0 +1,8 @@
+n=int(input())
+k=int(input())
+sum=0
+for x in range(1,k+1):
+  number=int(input())  
+  sum=sum+number
+print(sum)
+  
