ff3bc6541cfec60ec2fbe0c7a5123fcd5536a9d6
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-21 07:13:34 +0530

    Update Beginnerset12(2)printwheathernnumberisexistornot.py

--- beginnerlevel/Beginnerset12(2)printwheathernnumberisexistornot.py
+++ beginnerlevel/Beginnerset12(2)printwheathernnumberisexistornot.py
@@ -1,8 +1,8 @@
 N=int(input())
 K=int(input())
-for x in (N):
+for x in range (N):
   num=int(input())
-  if(num==K):
-    print('yes')
-  else:
-    print('no')
+if(num==K):
+  print('yes')
+else:
+  print('no')
