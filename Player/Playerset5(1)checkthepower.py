265172d12a7a4c1ceb08407e0105035bd190df6b
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-27 07:30:07 +0530

    Update Playerset5(1)checkthepower.py

--- Player/Playerset5(1)checkthepower.py
+++ Player/Playerset5(1)checkthepower.py
@@ -1,7 +1,10 @@
-N=int(input())
-K=int(input())
-for x in range of (K,N):
-  if(x*K==N):
-    print('yes')
-  else:
-    print('no')
+def power():
+  N=int(input())
+  K=int(input())
+  i=N
+  while(i>1):
+    i=N%K
+    N//=2
+  print(i==0)
+power()
+  
