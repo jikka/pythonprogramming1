a75052a82a187af56096a500a840d317150de9d7
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-30 10:32:56 +0530

    Create Hunterset1(10)arraysubset.py

---
+++ Hunte/Hunterset1(10)arraysubset.py
@@ -0,0 +1,22 @@
+Try:
+  N =int(input())
+  M=int(input())
+  A,B=[],[]
+  f=0
+  if(1<=N<=10000):
+    for i in range(N):
+      A.append(int(intput()))
+    for i in range(M):
+      B.append(int(input()))
+    for j in B:
+      if j in A:
+        continue
+      else:
+        f=1
+        break
+  if(f==1)
+    print('NO')
+  else:
+    print('YES')
+except:
+  print('Invalid')
