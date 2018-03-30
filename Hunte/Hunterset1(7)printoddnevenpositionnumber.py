eacbbb8c35bb1035f78ce89251856fffa70a5ce9
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-30 10:53:44 +0530

    Create Hunterset1(7)printoddnevenpositionnumber.py

---
+++ Hunte/Hunterset1(7)printoddnevenpositionnumber.py
@@ -0,0 +1,16 @@
+Try:
+  N=int(input())
+  ls=[]
+  if(1<=N<=100000):
+    for i in range(N):
+      ls. append(int(input())
+   odd,even=[],[]
+   for i in range(N):
+     if(i%2==0 and ls[i]%!=0):
+       even.append(ls[i])
+     if(i%2!=0 and ls[i]%==0)
+       odd. append(ls[i])
+  print(odd[],even[])
+except:
+    print('Invalid')
+     
