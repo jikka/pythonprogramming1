575d236de06b8964cd9d95fba02d67bb68bb870e
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-03-30 10:41:42 +0530

    Create Hunterset1(9)closetozero.py

---
+++ Hunte/Hunterset1(9)closetozero.py
@@ -0,0 +1,13 @@
+try:
+  ls=[]
+  N=int(input())
+  if(1<N=<=100000):
+    for i in range(N):
+      ls. append(int(input())
+    for i in range(N):
+      for j in range(i+1,N):
+        if(ls[i]+ls[j]==0):
+          print(ls[i],ls[j])
+except:
+  print('Invalid')
+    
