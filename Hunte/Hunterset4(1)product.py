9f4d35c603dcf216ba6f4584e905bd1d0e7d2e54
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-04-01 18:43:26 +0530

    Update Hunterset4(1)product.py

--- Hunte/Hunterset4(1)product.py
+++ Hunte/Hunterset4(1)product.py
@@ -1,10 +1,10 @@
 try:
   N=int(input())
   pro=1
-  if(N>=100000):
+  if(N<=100000):
     for i in range (N):
       number=int(input())
-      product=pro*number
-    print(product)
+      pro=pro*number
+    print(pro)
 except:
   print('invalid')
