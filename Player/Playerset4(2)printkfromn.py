199db17c831a54454c9a4db020bcce60c3a95e1a
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-04-05 14:07:34 +0530

    Create Playerset4(2)printkfromn.py

---
+++ Player/Playerset4(2)printkfromn.py
@@ -0,0 +1,13 @@
+try:
+  N=int(input())
+  K=int(inpur())
+  L=[]
+  if (1<=N<=1000000000000000000):
+    for i in range(N):
+      L.append(int(input())
+    if(K in L) :
+      print('yes')
+    else:
+      print('no')
+except
+  print('Invalid')
