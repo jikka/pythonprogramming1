1262cd44b4f1e3081cf7378ce98a8bc919624c1c
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-04-14 15:38:32 +0530

    Update Beginnerset12(5)printkthsmallestnumber.py

--- beginnerlevel/Beginnerset12(5)printkthsmallestnumber.py
+++ beginnerlevel/Beginnerset12(5)printkthsmallestnumber.py
@@ -1,6 +1,14 @@
-N=int(input())
-K=int(input())
-for x in range(N):
-  num=int(input())
-if( x==K):
-  print(num)
+def small2(l,K):
+	l.sort()
+	return l[K-1]
+def main():
+	try:
+		N=int(input())
+		K=int(input())
+		l=[]
+		for i in range(N):
+			l.append(int(input()))
+		print(small2(l,K))
+	except:
+		print('invalid')
+main()
