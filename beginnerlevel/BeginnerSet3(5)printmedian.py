02da9cb82c5f450b575f1f0de124bdd919f1341b
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-07-23 10:10:45 +0530

    Create BeginnerSet3(5)printmedian.py

---
+++ beginnerlevel/BeginnerSet3(5)printmedian.py
@@ -0,0 +1,17 @@
+imoort statistics
+n=int(input())
+ls=[]
+for i in range(0,n)
+  import statistics
+n=int(input())
+ls=[]
+for x in range(1,n+1):
+  number=int(input())
+  ls.append(number)
+print(statistics.median(ls))import statistics
+n=int(input())
+ls=[]
+for x in range(1,n+1):
+  number=int(input())
+  ls.append(number)
+print(statistics.median(ls))
