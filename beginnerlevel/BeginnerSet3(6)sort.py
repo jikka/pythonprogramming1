e0f033449db9de06d059917c8cdcfed56408a5ab
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-07-23 10:56:20 +0530

    Create BeginnerSet3(6)sort.py

---
+++ beginnerlevel/BeginnerSet3(6)sort.py
@@ -0,0 +1,11 @@
+try:
+  N=int(input())
+  ls=[]
+  if(1<=N>=100000):
+    for i in range(0,N):
+      number=int(input()
+      a=ls.append(number)
+  ls.sort()
+  print(ls)  
+  except:
+    print('Invalid')
