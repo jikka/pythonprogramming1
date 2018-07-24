c23c9dc888d4642ec147b2570c89c08a092b22d8
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-07-24 10:05:40 +0530

    Update HunterSet11(2)sumofthenumber.py

--- Hunte/HunterSet11(2)sumofthenumber.py
+++ Hunte/HunterSet11(2)sumofthenumber.py
@@ -1,11 +1,8 @@
-try:
-  N=int(input())
-  sum=0
-  if x in (1<=N<=100000):
-    while(N>0):
-      x=N%10
-      sum+=x
-      N=N//10
-  print(sum*sum)
-except:
-  print('Invalid')
+N=int(input())
+sum=0
+if (1<=N<=100000):
+  while(N>0):
+    x=N%10
+    sum+=x*x
+    N=N//10
+print(sum)
