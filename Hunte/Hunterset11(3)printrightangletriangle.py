34c37bf3facba4edaa0368ec886373df6e0c5452
Akshidhlavanya <36723131+Akshidhlavanya@users.noreply.github.com>
2018-07-24 11:17:48 +0530

    Update Hunterset11(3)printrightangletriangle.py

--- Hunte/Hunterset11(3)printrightangletriangle.py
+++ Hunte/Hunterset11(3)printrightangletriangle.py
@@ -1,9 +1,9 @@
 try:
   N=int(input())
-  for i in range(1,N+1):
-    for j in range(i):
-      print('1',end=' ')
+  for i in range(1,N*1):
+    if(i%2!=0):
+      for j in range(i):
+        print('1',end=' ')
     print()
-expect:
-  print('invalid')
-      
+except:
+  print('invalid')      
